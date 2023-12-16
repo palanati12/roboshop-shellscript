2664895a9d2842cd53e85345ec2d2f1e3ceb38ac
palanati12 <palanati12@github.com>
2023-12-16 11:43:58 +0530

    instaling redis service using shell script

--- Redis.sh
+++ Redis.sh
@@ -11,7 +11,7 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 VALIDATE(){
-if [ $1 -ne 0 ] 
+if [ $1 -ne 0 ]
 then 
     echo -e "ERROR:: $2 ....$R is failed $N"
 else
@@ -19,7 +19,7 @@ else
 fi           
 }
 
-if [ $ID -ne 0 ]
+if [ $ID -ne 0 ]:
 then
     echo -e "$R ERROR::please run the script with root access $N"
     exit 1
